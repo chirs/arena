86d716be66958bd9b46725669d51da3629ae93bf
chirs <chrisedgemon@gmail.com>
2013-02-18 18:27:09 -0500

    Work to make supervisor support more than one simultaneous game.

--- plumbing/supervisor.py
+++ plumbing/supervisor.py
@@ -8,7 +8,7 @@ from gameplay.tictactoe import TicTacToe
 from gameplay.checkers import Checkers
 from gameplay.connect4 import ConnectFour
 
-GAMES = {'tictactoe':TicTacToe, 'checkers':Checkers, 'connectfour':ConnectFour}
+
 
 def build_state(player, board, winner, history):
     return {
@@ -18,45 +18,79 @@ def build_state(player, board, winner, history):
         'history': history,
         }
 
-def play(sockets, game_class):
 
-    player = 1
-    game = game_class()
-    history = []
+GAMES = {
+        'tictactoe': TicTacToe, 
+        'checkers': Checkers, 
+        'connectfour':ConnectFour,
+        }
+
+
+def supervise(host, port):
+
+    active_games = {}
+
+    sock.bind((host, port))
+    print("Accepting connection on host", socket.gethostname(), ", port ", port)
+    sock.listen(200) 
+
+    def end_game(gid, result):
+        d = active_games[gid]
+        send_json(d['players'][1], build_state(1, d.game.board, result, d.game.history))
+        send_json(d['players'][2], build_state(2, d.game.board, result, d.game.history))
+        active_games.pop(gid)
+
 
     while True:
-        player_sock = sockets[player]
-        state = build_state(player, game.board, 0, history)
-        send_json(player_sock, state)
 
-        move = get_json(player_sock)
+        # New client logic.
+        player_socket, _ = sock.accept() # This breaks the code since accept is blocking. (pretend it is non-blocking.)
+        handshake = player_socket.recv(1028).decode()
+        handshake_json = json.loads(handshake)
+        game_string = handshake_json['game']
+        game_class = game_classes[game_string]
 
-        if game.move_legal(move):
+        valid_games = [gid for gid, v in active_games.items() if v['players'][2] == None and v['name'] == game_string]
 
-            player = 3 - player # Toggle between 1 and 2...
-            history.append(move)
-            game.draw_board()
-            game.transition(move, player)
+        if valid_games:
+            # Player 2 logic
+            g = valid_games[0]
+            g['players'][2] = player_socket
+            send_json(g['player1'], {'gid':gid, 'player':1})
 
-            result = game.result()
-            if result:
-                game.draw_board()
-                print("Game over!")
-                send_json(sockets[1], build_state(1, game.board, result, history))
-                send_json(sockets[2], build_state(2, game.board, result, history))
-                return
+        else:
+            gid = datetime.datetime.now().isoformat()
+            d = {
+                'game': game_class(),
+                'name': game_string,
+                'players' {
+                    1: player_socket,
+                    2: None,
+                    },
+                'history': [],
+                }
+            active_games[gid] = d
 
-def play_wrapper(host, port, game):
-    sockets = start(host, port)
-    params1 = get_json(sockets[1])
-    params2 = get_json(sockets[2])
-    send_json(sockets[1], {'game':game, 'player':1})
-    send_json(sockets[2], {'game':game, 'player':2})
-    print("Game starts!\n")
-    play(sockets, GAMES[game.lower()])
-    stop(sockets)
+        for gid, values in active_games.items():
+            g = values['game']
+            sock = values['players'][g.current_player]
+            move = get_json(sock)
+            if game.move_legal(move):
+                g['history'].append(move)
+                game.transition(move, player)
+
+                result = game.result()
+                if result:
+                    print("Game over!")
+                    end_game(gid, result)
+
+            else:
+                print("Game over. Player %s forfeits because of illegal move." % g.current_player)
+                winner = 3 - g.current_player
+                end_game(gid, winner)
+
 
 if __name__ == "__main__":
-    [_, game, host, port] = sys.argv
-    play_wrapper(host, int(port), game)
-
+    [_, host, port] = sys.argv
+    supervise(host, port)
+ 
