7edf6456b6cb2a919073ba92e46b2f4cd821d4e1
Thomas Ballinger <thomasballinger@gmail.com>
2013-02-23 13:25:06 -0500

    more concise solutions checking, syntax sugar

--- gameplay/connect4.py
+++ gameplay/connect4.py
@@ -18,6 +18,7 @@ class ConnectFour(Game):
 
     player_mapping = {1: 'b', 2: 'r'}
     color_mapping = {v: k for k, v in player_mapping.items()}
+    cached_solutions = None
 
     def __init__(self, board=' '*42, current_player=1):
         self.board = board
@@ -27,119 +28,54 @@ class ConnectFour(Game):
         s = '\n'.join([self.board[i:i+7] for i in range(0, 42, 7)] + ['='*7])
         print(s)
 
-    def move_is_legal(self, move):
-        """Check that the top field for the move column is open."""
-        return self.board[move] == ' '
+    def move_is_legal(self, column):
+        """Check that the top field for the column is open."""
+        return self.board[column] == ' '
 
-    def transition(self, move, player):
-        coords = [(e, move) for e in range(5, -1,-1)]
-        indexes = [coords2index(e) for e in coords]
+    def transition(self, column, player):
+        assert player == self.current_player
+        coords = [(row, column) for row in reversed(range(6))]
+        indexes = [coords2index(row_coord) for row_coord in coords]
         for i in indexes:
             if self.board[i] == ' ':
-                bl = list(self.board)
-                bl[i] = self.player_mapping[player]
-                self.board = ''.join(bl)
-                self.current_player = 3 - self.current_player
+                self.board = self.board[:i] + self.player_mapping[player] + self.board[i:]
+                self.current_player = 3 - player
                 return
-        raise Exception("Logic Error")
+        raise ValueError('column %d is full' % column)
 
     def is_tie(self):
-        empty_spaces = [e for e in self.board if e == ' ']
-        return not empty_spaces
+        return not [e for e in self.board if e == ' ']
 
-
-
-    def check_cells(self, l):
-        last = None
-        consecutive = 0
-        for e in l:
-            if e != last:
-                last = e
-                consecutive = 1
-            else:
-                consecutive += 1
-
-            if consecutive == 4:
-                return True
-
-        return False
-            
-        
-        
-
-    def check_horizontal(self, index):
-        value = self.board[index]
-        if value.strip() == '':
-            return False
-
-        x, y = index2coords(index)
-        coord_list = [(x, y+e) for e in range(-3, 4)]
-        indexes = [coords2index(e) for e in coord_list if is_valid(e)]
-
-        try:
-            return self.check_cells([self.board[i] for i in indexes])
-        except:
-            import pdb; pdb.set_trace()
-        x = 5
-
-
-    def check_vertical(self, index):
-
-        value = self.board[index]
-        if value.strip() == '':
-            return False
-
-        x, y = index2coords(index)
-        coord_list = [(x+e, y) for e in range(-3, 4)]
-        indexes = [coords2index(e) for e in coord_list if is_valid(e)]
-        return self.check_cells([self.board[i] for i in indexes])
-
-    def check_diagonal1(self, index):
-
-        value = self.board[index]
-        if value.strip() == '':
-            return False
-
-        x, y = index2coords(index)
-        coord_list = [(x+e, y+e) for e in range(-3, 4)]
-        indexes = [coords2index(e) for e in coord_list if is_valid(e)]
-        return self.check_cells([self.board[i] for i in indexes])
-
-    def check_diagonal2(self, index):
-
-        value = self.board[index]
-        if value.strip() == '':
-            return False
-
-        x, y = index2coords(index)
-        coord_list = [(x-e, y+e) for e in range(-3, 4)]
-        indexes = [coords2index(e) for e in coord_list if is_valid(e)]
-        return self.check_cells([self.board[i] for i in indexes])
-
-
-    def result_for_cell(self, index):
-        if self.board[index] == ' ':
-            return False
-
-        return self.check_horizontal(index) or\
-            self.check_vertical(index) or\
-            self.check_diagonal1(index) or \
-            self.check_diagonal2(index)
-            
-        
-
+    @classmethod
+    def get_solutions(cls):
+        if cls.cached_solutions is not None:
+            return cls.cached_solutions
+        seq = lambda start, end: range(start, end+1)
+        rows = 6
+        columns = 7
+        run = 4
+        horizontal_solutions = [[(start_r, c) for c in range(start_c, start_c+run)]
+                                   for start_r in range(rows)
+                                   for start_c in seq(0, columns-run)]
+        vertical_solutions = [[(r, start_c) for r in range(start_r, start_r+run)]
+                                   for start_r in seq(0, rows-run)
+                                   for start_c in range(columns)]
+        diagonal_solutions_down_right = [[(r, c) for r,c in zip(range(start_r, start_r+run), range(start_c, start_c+run))]
+                                   for start_r in seq(0, rows-run)
+                                   for start_c in seq(0, columns-run)]
+        diagonal_solutions_down_left = [[(r, c) for r,c in zip(range(start_r, start_r+run), reversed(range(start_c, start_c+run)))]
+                                   for start_r in seq(0, rows-run)
+                                   for start_c in seq(0, columns-run)]
+        solutions = sum([horizontal_solutions, vertical_solutions, diagonal_solutions_down_right, diagonal_solutions_down_left], [])
+        cls.cached_solutions = [[coords2index(pos) for pos in solution] for solution in solutions]
+        return cls.cached_solutions
 
     def result(self):
         if self.is_tie():
             return -1
         else:
-            for index in range(0, 42):
-                result = self.result_for_cell(index)
-                if result:
-                    color = self.board[index]
+            for solution in self.get_solutions():
+                if all(self.board[index] for index in solution):
+                    color = self.board[solution[0]]
                     return self.color_mapping[color]
-
             return 0
-
-
-
